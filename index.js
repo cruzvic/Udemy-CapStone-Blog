d42d73dc0ecc05b9f825e9c8679805977a97d11b
Victor Cruz <victorhugo.cruzdasilva@uzh.ch>
2025-03-26 21:36:54 +0100

    Add blog post creation and overview functionality
    - Implemented blog post constructor and storage
    - Added routes for creating and viewing blog posts
    - Updated views to display blog posts and creation form
    - Integrated body-parser for form handling

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import morgan from 'morgan';
+import bodyParser from 'body-parser';
 
 const app = express();
 const port = 3000;
 
 let blogPosts = [];
+let id_number = 1;
 
 // Constructor for Blog Posts
 function blogPost(title, author, text) {
@@ -12,25 +14,51 @@ function blogPost(title, author, text) {
     this.author = author;
     this.text = text;
     this.creationDate = new Date();
+    this.blogID = id_number++;
+
 };
+
+// Function to add a new blog post
+function addBlogPost(title, author, text) {
+    const newPost = new blogPost(title, author, text);
+    blogPosts.push(newPost);
+}
 
 // MIDDLEWARES
 app.use(express.static("public"));
 app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // ROUTES
 app.get("/", (req, res) => {
-    res.render("home.ejs");
+    res.render("home.ejs", {
+        blogPosts: blogPosts
+    });
 });
 
 app.get("/post/:postID", (req, res) => {
-    // TODO: GET POST OVERVIEW PAGE
-    res.render("blogOverview.ejs");
+    const blogID = req.params.postID;
+    const post = blogPosts.find((post) => post.blogID == blogID);
+    console.log(post);
+    if (post) {
+        res.render("blogOverview.ejs", {
+            blogPost: post
+        });
+    } else {
+        res.status(404).send("Post not found");
+    }
 });
 
-app.post("/post", (req, res) => {
-    // TODO: CREATE POST
+app.get("/create-post", (req, res) => {
     res.render("blogCreation.ejs");
+});
+
+app.post("/create-post", (req, res) => {
+    addBlogPost(req.body.title, req.body.author, req.body.text);
+    console.log(blogPosts);
+    res.render("home.ejs", {
+        blogPosts: blogPosts
+    });
 });
 
 app.put("/post/:postId", (req, res) => {
