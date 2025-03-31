9775b8e128f1ee69e39b880993f5696682050494
Victor Cruz <victorhugo.cruzdasilva@uzh.ch>
2025-03-31 09:05:16 +0200

    Update server port configuration to use environment variable with fallback

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import bodyParser from 'body-parser';
 import methodOverride from 'method-override';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT | 3000;
 
 let blogPosts = [];
 let id_number = 1;
